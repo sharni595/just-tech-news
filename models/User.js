8c2473ce759d67d64a0c8fc582b96d31b21fba32
Sharni Zaugg <sharni.greener@gmail.com>
2021-06-01 12:35:23 -0600

    create hashed passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create our User model
 class User extends Model {}
@@ -43,8 +44,16 @@ User.init(
         }
     },
     {
-        //TABLE CONFIGURATION OPTIONS GO HERE
-
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData){
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         //pass in our imported sequelize connection
         sequelize,
         //don't automatically create createdAt/updatedAt timestamp fields
