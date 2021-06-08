c038af8c2de2b4e75df1b0ceb251043dab8d74fd
Sharni Zaugg <sharni.greener@gmail.com>
2021-06-07 22:30:53 -0600

    add cookies and log out

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,6 +34,22 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
+});
+
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
+    res.render('login')
+})
+
+router.get('/', (req, res) => {
+    console.log(req.session);
+
+    //other logic...
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,9 +4,25 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +33,7 @@ app.set('view engine', 'handlebars');
 
 //turn on routes
 app.use(routes);
+
 
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
